a2d6cad8930323da3cb1e1173e1baf2c31d8d516
Sridharan Murugadass <sridharanbe.ece@gmail.com>
2018-10-12 15:52:16 +0530

    Update EmailController.java

--- src/main/java/com/gogo/service/EmailController.java
+++ src/main/java/com/gogo/service/EmailController.java
@@ -66,101 +66,103 @@ public class EmailController {
 	              "<meta charset=\"utf-8\">\n" + 
 	              "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" + 
 	              "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" + 
-	              "<title>Example of Bootstrap 3 Simple Tables</title>\n" + 
+	              "<title>Weekly Reset Report</title>\n" + 
+	              "<h1 align=\"center\" style=\"color:brown;\" style=\"background-color:powderblue;\"> Weekly Reset Report </h1><br>\n" + 
+	              "<h5 align=\"center\" style=\"color:blue;\" style=\"background-color:powderblue;\"> Reset Values = No Of Reset / Flight Hours</h5><br>\n" + 
 	              "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">\n" + 
 	              "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js\"></script>\n" + 
 	              "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>\n" + 
 	              "<style type=\"text/css\">\n" + 
-	              "   .bs-example{\n" + 
-	              "       margin: 20px;\n" + 
-	              "   }\n" + 
+	              "  .bs-example{\n" + 
+	              "    margin: 20px;\n" + 
+	              "  }\n" + 
 	              "a.tooltip {outline:none; }\n" + 
 	              "a.tooltip strong {line-height:30px;}\n" + 
 	              "a.tooltip:hover {text-decoration:none;} \n" + 
 	              "a.tooltip span {\n" + 
-	              "    z-index:10;display:none; padding:14px 20px;\n" + 
-	              "    margin-top:-30px; margin-left:28px;\n" + 
-	              "    width:300px; line-height:16px;\n" + 
+	              "  z-index:10;display:none; padding:14px 20px;\n" + 
+	              "  margin-top:-30px; margin-left:28px;\n" + 
+	              "  width:300px; line-height:16px;\n" + 
 	              "}\n" + 
 	              "a.tooltip:hover span{\n" + 
-	              "    display:inline; position:absolute; color:#111;\n" + 
-	              "    border:1px solid #DCA; background:#fffAF0;}\n" + 
+	              "  display:inline; position:absolute; color:#111;\n" + 
+	              "  border:1px solid #DCA; background:#fffAF0;}\n" + 
 	              ".callout {z-index:20;position:absolute;top:30px;border:0;left:-12px;}\n" + 
-	              "    \n" + 
+	              "  \n" + 
 	              "/*CSS3 extras*/\n" + 
 	              "a.tooltip span\n" + 
 	              "{\n" + 
-	              "    border-radius:4px;\n" + 
-	              "    box-shadow: 5px 5px 8px #CCC;\n" + 
+	              "  border-radius:4px;\n" + 
+	              "  box-shadow: 5px 5px 8px #CCC;\n" + 
 	              "}\n" + 
 	              "tr .spnTooltip {\n" + 
-	              "    z-index:10;display:none; padding:14px 20px;\n" + 
-	              "    margin-top:-30px; margin-left:28px;\n" + 
-	              "    width:300px; line-height:16px;\n" + 
+	              "  z-index:10;display:none; padding:14px 20px;\n" + 
+	              "  margin-top:-30px; margin-left:28px;\n" + 
+	              "  width:300px; line-height:16px;\n" + 
 	              "}\n" + 
 	              "tr:hover .spnTooltip{\n" + 
-	              "    display:inline; position:absolute; color:#111;\n" + 
-	              "    border:1px solid #DCA; background:#fffAF0;}\n" + 
+	              "  display:inline; position:absolute; color:#111;\n" + 
+	              "  border:1px solid #DCA; background:#fffAF0;}\n" + 
 	              ".callout {z-index:20;position:absolute;top:30px;border:0;left:-12px;}\n" + 
 	              "</style>\n" + 
 	              "</head>\n" + 
 	              "<body>\n" + 
 	              "<div class=\"bs-example\">\n" + 
-	              "   <table class=\"table\">\n" + 
-	              "       <thead>\n" + 
-	              "           <tr>\n" + 
-	              "               <th>Total Ratio</th>\n" + 
-	              "               <th>Airlines</th>\n" + 
-	              "               <th>Monday</th>\n" + 
-	              "               <th>Tuesday</th>\n" + 
-	              "               <th>Wednesday</th>\n" + 
-	              "               <th>Thursady</th>\n" + 
-	              "               <th>Friday</th>\n" + 
-	              "           </tr>\n" + 
-	              "       </thead>\n" + 
-	              "       <tbody>\n" + 
-	              "           <tr>\n" + 
-	              "               <td><span class=\"spnDetails\">0.3</span><span class=\"spnTooltip\">\n" + 
-	              "<strong>Current reset - 100</strong><br/>\n" + 
-	              "        Previous week reset -90\n" + 
-	              "        <br/> Flight Hours - 14 </br> Re-set Flight hours - 2\n" + 
-	              "    </span>\n" + 
+	              "  <table class=\"table\">\n" + 
+	              "    <thead>\n" + 
+	              "      <tr>\n" + 
+	              "        <th>Airlines</th>\n" + 
+	              "        <th>Reset ratio </br> all weeks</th>\n" + 
+	              "        <th>Monday </br> reset ratio </th>\n" + 
+	              "        <th>Tuesday</br> reset ratio </th>\n" + 
+	              "        <th>Wednesday</br> reset ratio </th>\n" + 
+	              "        <th>Thursady</br> reset ratio </th>\n" + 
+	              "        <th>Friday</br> reset ratio </th>\n" + 
+	              "      </tr>\n" + 
+	              "    </thead>\n" + 
+	              "    <tbody>\n" + 
+	              "      <tr>\n" + 
+	              "         <td >DAL</td>\n" + 
+	              "        <td><span class=\"spnDetails\">0.3</span><span class=\"spnTooltip\">\n" + 
+	              "    Current reset - 100<br/>\n" + 
+	              "    Previous week reset -90\n" + 
+	              "    <br/> Flight Hours - 14 </br> Re-set Flight hours - 2\n" + 
+	              "  </span>\n" + 
 	              "</td>\n" + 
-	              "               <td >DAL</td>\n" + 
-	              "               <td>0.1</td>\n" + 
-	              "                <td>0.4</td>\n" + 
-	              "               <td>0.3</td>\n" + 
-	              "               <td>0.3</td>\n" + 
-	              "               <td>0.3</td>\n" + 
-	              "           </tr>\n" + 
-	              "           <tr>\n" + 
-	              "               <td><span class=\"spnDetails\">0.2</span><span class=\"spnTooltip\">\n" + 
-	              "<strong>Current reset - 105</strong><br/>\n" + 
-	              "        Previous week reset -95\n" + 
-	              "        <br/> Flight Hours - 14 </br> Re-set Flight hours - 2\n" + 
-	              "    </span>\n" + 
-	              "               <td>ASA</td>\n" + 
-	              "               <td>0.4</td>\n" + 
-	              "               <td>0.3</td>\n" + 
-	              "               <td>0.3</td>\n" + 
-	              "               <td>0.3</td>\n" + 
-	              "               <td>0.3</td>\n" + 
-	              "           </tr>\n" + 
-	              "           <tr>\n" + 
-	              "               <td><span class=\"spnDetails\">0.1</span><span class=\"spnTooltip\">\n" + 
-	              "<strong>Current reset - 40</strong><br/>\n" + 
-	              "        Previous week reset -20\n" + 
-	              "        <br/> Flight Hours - 14 </br> Re-set Flight hours - 2\n" + 
-	              "    </span>\n" + 
-	              "               <td>AAL</td>\n" + 
-	              "                <td>0.4</td>\n" + 
-	              "               <td>0.3</td>\n" + 
-	              "               <td>0.3</td>\n" + 
-	              "               <td>0.3</td>\n" + 
-	              "               <td>0.3</td>\n" + 
-	              "           </tr>\n" + 
-	              "       </tbody>\n" + 
-	              "   </table>\n" + 
+	              "        <td>0.1</td>\n" + 
+	              "        <td>0.4</td>\n" + 
+	              "        <td>0.3</td>\n" + 
+	              "        <td>0.3</td>\n" + 
+	              "        <td>0.3</td>\n" + 
+	              "      </tr>\n" + 
+	              "      <tr>\n" + 
+	              "         <td>ASA</td>\n" + 
+	              "        <td><span class=\"spnDetails\">0.2</span><span class=\"spnTooltip\">\n" + 
+	              "Current reset - 105<br/>\n" + 
+	              "    Previous week reset -95\n" + 
+	              "    <br/> Flight Hours - 14 </br> Re-set Flight hours - 2\n" + 
+	              "  </span>\n" + 
+	              "        <td>0.4</td>\n" + 
+	              "        <td>0.3</td>\n" + 
+	              "        <td>0.3</td>\n" + 
+	              "        <td>0.3</td>\n" + 
+	              "        <td>0.3</td>\n" + 
+	              "      </tr>\n" + 
+	              "      <tr>\n" + 
+	              "        <td>AAL</td>\n" + 
+	              "        <td><span class=\"spnDetails\">0.1</span><span class=\"spnTooltip\">\n" + 
+	              "Current reset - 40<br/>\n" + 
+	              "    Previous week reset -20\n" + 
+	              "    <br/> Flight Hours - 14 </br> Re-set Flight hours - 2\n" + 
+	              "  </span>\n" + 
+	              "        <td>0.4</td>\n" + 
+	              "        <td>0.3</td>\n" + 
+	              "        <td>0.3</td>\n" + 
+	              "        <td>0.3</td>\n" + 
+	              "        <td>0.3</td>\n" + 
+	              "      </tr>\n" + 
+	              "    </tbody>\n" + 
+	              "  </table>\n" + 
 	              "</div>\n" + 
 	              "</body>\n" + 
 	              "</html>",
